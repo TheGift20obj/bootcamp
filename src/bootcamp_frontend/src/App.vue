cb8c3452a95a94ef76ed05ac04275c1e90cd5258
TheGift20obj <djadunov@gmail.com>
2024-06-24 19:10:20 +0200

    Added counter

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -16,13 +16,24 @@ async function handleSubmit(e) {
 <template>
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
+    Siema!
     <br />
-    <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
+    <button @click="podbij">podbij</button>
+      {{ licznik }}
   </main>
+
+  <script>
+  export default {
+    data() {
+      return {
+        licznik: 0
+      }
+    }
+    methods: {
+      podbij() {
+        this.licznik++
+      }
+    }
+  }
+  </script>
 </template>
